e8661c619a7347a18538649ffb582d74286656dc
rod12151 <rodmel.calderon.27@unsch.edu.pe>
2023-04-18 09:02:31 -0500

    dtos for reserbation

---
+++ src/main/java/com/rodmel/best_travel/api/models/responses/ReservationResponse.java
@@ -0,0 +1,27 @@
+package com.rodmel.best_travel.api.models.responses;
+
+import com.rodmel.best_travel.domain.entities.HotelEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ReservationResponse implements Serializable {
+    private UUID id;
+    private LocalDateTime dateTimeReservation;
+    private LocalDate dateStart;
+    private LocalDate dateEnd;
+    private Integer totalDays;
+    private BigDecimal price;
+    private HotelEntity hotel;
+}
