913be49ad944d35ac46a3a38d1777a46e42a6c43
rod12151 <rodmel.calderon.27@unsch.edu.pe>
2023-04-21 11:00:03 -0500

    definiendo hotelServicesand Controllers

---
+++ src/main/java/com/rodmel/best_travel/infraestructure/services/HotelService.java
@@ -0,0 +1,71 @@
+package com.rodmel.best_travel.infraestructure.services;
+
+import com.rodmel.best_travel.api.models.responses.HotelResponse;
+import com.rodmel.best_travel.domain.entities.HotelEntity;
+import com.rodmel.best_travel.domain.repositories.HotelRepository;
+import com.rodmel.best_travel.infraestructure.abstract_services.IHotelService;
+import com.rodmel.best_travel.util.SortType;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Transactional
+@Service
+@Slf4j
+@AllArgsConstructor
+
+public class HotelService implements IHotelService {
+
+    private final HotelRepository hotelRepository;
+    @Override
+    public Page<HotelResponse> realAll(Integer page, Integer size, SortType sortType) {
+        PageRequest pageRequest = null;
+        switch (sortType){
+            case NONE -> pageRequest = PageRequest.of(page,size);
+            case LOWER -> pageRequest = PageRequest.of(page,size, Sort.by(FIELD_BY_SORT).ascending());
+            case UPPER -> pageRequest = PageRequest.of(page, size,Sort.by(FIELD_BY_SORT).descending());
+        }
+        return this.hotelRepository.findAll(pageRequest).map(this::entityToResponse);
+
+    }
+
+    @Override
+    public Set<HotelResponse> readLessPrice(BigDecimal price) {
+        return this.hotelRepository.findByPriceLessThan(price)
+                .stream()
+                .map(this::entityToResponse)
+                .collect(Collectors.toSet());
+
+    }
+
+    @Override
+    public Set<HotelResponse> readBetweenPrices(BigDecimal min, BigDecimal max) {
+        return this.hotelRepository.findByPriceIsBetween(min, max)
+                .stream()
+                .map(this::entityToResponse)
+                .collect(Collectors.toSet());
+
+    }
+
+    @Override
+    public Set<HotelResponse> readByRating(Integer rating) {
+        return this.hotelRepository.findByRatingGreaterThan(rating)
+                .stream()
+                .map(this::entityToResponse)
+                .collect(Collectors.toSet());
+    }
+    private HotelResponse entityToResponse(HotelEntity entity){
+        HotelResponse response = new HotelResponse();
+        BeanUtils.copyProperties(entity,response);
+        return response;
+    }
+}
