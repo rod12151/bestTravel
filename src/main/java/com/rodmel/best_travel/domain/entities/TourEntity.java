61e782215a881bae92401cc76aa18e39ba2a04a3
rod12151 <rodmel.calderon.27@unsch.edu.pe>
2023-04-24 22:41:46 -0500

    usando anotacion prePersist

--- src/main/java/com/rodmel/best_travel/domain/entities/TourEntity.java
+++ src/main/java/com/rodmel/best_travel/domain/entities/TourEntity.java
@@ -40,33 +40,9 @@ public class TourEntity {
     @ManyToOne
     @JoinColumn(name = "id_customer" )
     private CustomerEntity customer;
-
-    //joins inversos
-    public void addTicket(TicketEntity ticket) {
-        if (Objects.isNull(this.tickets))this.tickets = new HashSet<>();
-        this.tickets.add(ticket);
-    }
-
-    public void removeTicket(UUID id) {
-        if (Objects.isNull(this.tickets)) this.tickets = new HashSet<>();
-        this.tickets.removeIf(ticket -> ticket.getId().equals(id));
-    }
-
-    public void updateTickets() {
+    @PrePersist
+    public void upDateFk(){
         this.tickets.forEach(ticket -> ticket.setTour(this));
-    }
-
-    public void addReservation(ReservationEntity reservation) {
-        if (Objects.isNull(this.reservations)) this.reservations = new HashSet<>();
-        this.reservations.add(reservation);
-    }
-
-    public void removeReservation(UUID idReservation){
-        if (Objects.isNull(this.reservations))this.reservations = new HashSet<>();
-        this.reservations.removeIf(r -> r.getId().equals(idReservation));
-
-    }
-    public void updateReservations(){
-        this.reservations.forEach((r -> r.setTour(this)));
+        this.reservations.forEach(reservation -> reservation.setTour(this));
     }
 }
