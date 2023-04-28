f0911614a7dc663093f26c129823c49ad940439c
rod12151 <rodmel.calderon.27@unsch.edu.pe>
2023-04-28 11:01:19 -0500

    borrado de cache

--- src/main/java/com/rodmel/best_travel/util/constants/CacheConstants.java
+++ src/main/java/com/rodmel/best_travel/util/constants/CacheConstants.java
@@ -4,5 +4,7 @@ public class CacheConstants {
 
     public static final String FLY_CACHE_NAME = "flights";
     public static final String HOTEL_CACHE_NAME = "hotels";
+
+    //clean cache everyday to 12:00 am
     public static final String SCHEDULED_RESET_CACHE = "0 0 0 * * ?";
 }
